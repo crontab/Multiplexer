0ed7f59864a72fea50ecb1065e769d1781b21cf9
Hovik Melikyan <hovik.melikyan@gmail.com>
2019-09-29 13:24:58 +0200

    Multiplexer Framework project (iOS)

---
+++ Multiplexer/Multiplexer.h
@@ -0,0 +1,19 @@
+//
+//  Multiplexer.h
+//  Multiplexer
+//
+//  Created by Hovik Melikyan on 29/09/2019.
+//  Copyright © 2019 Hovik Melikyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Multiplexer.
+FOUNDATION_EXPORT double MultiplexerVersionNumber;
+
+//! Project version string for Multiplexer.
+FOUNDATION_EXPORT const unsigned char MultiplexerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Multiplexer/PublicHeader.h>
+
+#import <CommonCrypto/CommonHMAC.h>
