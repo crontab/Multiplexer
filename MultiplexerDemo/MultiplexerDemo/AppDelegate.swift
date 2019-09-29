5ec8ca84781a4baa71d5a00de292953078bfe6e7
Hovik Melikyan <hovik.melikyan@gmail.com>
2019-09-29 16:40:32 +0200

    Demo project, initial commit

---
+++ MultiplexerDemo/MultiplexerDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MultiplexerDemo
+//
+//  Created by Hovik Melikyan on 29/09/2019.
+//  Copyright © 2019 Hovik Melikyan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
---
+++ MultiplexerDemo/MultiplexerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiplexerDemo
+//
+//  Created by Hovik Melikyan on 29/09/2019.
+//  Copyright © 2019 Hovik Melikyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
