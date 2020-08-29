64dcd8acbcc57c8574ea0f1818ff15098f661392
Hovik Melikyan <hovik.melikyan@gmail.com>
2020-08-29 20:52:05 +0200

    Synchronization shortcuts

--- Multiplexer/Common.swift
+++ Multiplexer/Common.swift
@@ -22,16 +22,32 @@ public func DLOG(_ s: String) {
 }
 
 
-// MARK: - Synchronization
+// MARK: - Synchronization shortcuts
 
 
+/// Asynchronously execute a block on the main thread
 @inlinable
-public func Async(_ execute: @escaping () -> Void) {
+func Async(_ execute: @escaping () -> Void) {
 	DispatchQueue.main.async(execute: execute)
 }
 
 
+/// Execute on a new thread with a given QoS
 @inlinable
-public func AsyncAfter(_ after: TimeInterval, _ execute: @escaping () -> Void) {
+func AsyncGlobal(qos: DispatchQoS.QoSClass = .default, _ execute: @escaping () -> Void) {
+	DispatchQueue.global(qos: qos).async(execute: execute)
+}
+
+
+/// Execute on the main thread with a delay
+@inlinable
+func Async(after: TimeInterval, _ execute: @escaping () -> Void) {
+	DispatchQueue.main.asyncAfter(deadline: .now() + after, execute: execute)
+}
+
+
+/// Execute a work item on the main thread with a delay; work item can be cancelled
+@inlinable
+func Async(after: TimeInterval, _ execute: DispatchWorkItem) {
 	DispatchQueue.main.asyncAfter(deadline: .now() + after, execute: execute)
 }
