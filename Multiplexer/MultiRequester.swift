b68716bd17ad08f527b513b7750c6aec8915b9ff
Hovik Melikyan <hovik.melikyan@gmail.com>
2021-10-10 13:39:57 +0200

    MultiRequester: a fix and some comments

--- Multiplexer/MultiRequester.swift
+++ Multiplexer/MultiRequester.swift
@@ -9,17 +9,20 @@
 import Foundation
 
 
+/// EXPERIMENTAL
+/// If your backend supports multiple-ID requests (such as  e.g.`/profiles/[id1,id2]`), then MultiRequester can be used in combination with an existing MultiplexerMap object to combine single and multi-requests into the same caching infrastructure. Multi-ID requests made via MultiRequester's `request(...)` method can update the map linked to it and also reuse the cached values stored by the map. Thus, objects will be cached locally regardless of whether they were requested via singular endpoints or multi-ID ones; and on the other hand, multi-ID requests can save bandwidth by reusing some of the objects already cached and requesting fewer ID's (or even none) from the backend.
+
 open class MultiRequester<K: LosslessStringConvertible, T: Codable & Identifiable> where T.ID == K {
 
 	public typealias OnMultiResult = (Result<[T], Error>) -> Void
 
-	public init(map: MultiplexerMap<K, T>, onMultiFetch: @escaping ([K], OnMultiResult) -> Void) {
+	public init(map: MultiplexerMap<K, T>, onMultiFetch: @escaping ([K], @escaping OnMultiResult) -> Void) {
 		self.multiplexerMap = map
 		self.onMultiFetch = onMultiFetch
 	}
 
 
-	/// Attempts to retrieve objects associated with the set of keys [K]. The number of the results is not guaranteed to be the same as the number of keys, neither is the order guaranteed to be the same. If the fetcher returns an error, this function may return some number of previously cached results but will also return the error object (it's why the completion has both result and error arguments). The result set is not optional but may be empty if neither the fetcher nor the caching system have any new values.
+	/// This method attempts to retrieve objects associated with the set of keys [K]. The number of the results is not guaranteed to be the same as the number of keys, neither is the order guaranteed to be the same. If the fetcher returns an error, this function may return some number of previously cached results but will also return the error object (it's why the completion has both result and error arguments). The result set is not optional but may be empty if neither the fetcher nor the caching system have any new values.
 	public func request(keys: [K], completion: (([T], Error?) -> Void)?) {
 
 		// See if there are any good non-expired cached values in the map object; also build the set of keys to be used in a call to the user's fetcher function.
